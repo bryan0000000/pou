d53d1bfeeaa9182298e705f3a9e9053bb7fb5204
pou-1 <borelabryan947@gmail.com>
2024-03-27 11:53:03 -0300

    dia 1 abc_action_bar_item_background_material

---
+++ pou/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace pou;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ pou/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace pou;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
